74656439f5f9c8e32fa8efd7aefb80021d9ed781
sajmon <nowak.sajmon@gmail.com>
2021-05-18 14:37:15 +0200

    changed geocoding link

--- src/modules/GeocodingAPI.js
+++ src/modules/GeocodingAPI.js
@@ -3,7 +3,7 @@ const APIKey = "d00251ce110d25d4425b2eede28a4a09";
 
 export default async function getCoordinates(city) {
   const response = await fetch(
-    `http://api.openweathermap.org/geo/1.0/direct?q=${city}&appid=${APIKey}`
+    `https://api.openweathermap.org/geo/1.0/direct?q=${city}&appid=${APIKey}`
   );
   const data = await response.json();
   return data[0];
